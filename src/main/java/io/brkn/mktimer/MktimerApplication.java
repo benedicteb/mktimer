d04be7ce0ccef9605cf43cb5cd5eb6fa0672bf06
Benedicte Emilie Brækken <b@brkn.io>
2017-12-04 17:32:06 +0100

    Initial commit

---
+++ src/main/java/io/brkn/mktimer/MktimerApplication.java
@@ -0,0 +1,12 @@
+package io.brkn.mktimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MktimerApplication.class, args);
+	}
+}
