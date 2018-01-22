15f5069894fd0d5554fdf54e418f6db7c6bd60b7
Benedicte Emilie Brækken <b@brkn.io>
2018-01-22 17:29:45 +0100

    Add before and after query params to activity

--- mktimer-backend/src/main/java/io/brkn/mktimer/repository/ActivityRepository.java
+++ mktimer-backend/src/main/java/io/brkn/mktimer/repository/ActivityRepository.java
@@ -4,9 +4,17 @@ import io.brkn.mktimer.domain.Activity;
 import io.brkn.mktimer.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
     List<Activity> findAllByCategory(Category category);
     List<Activity> findAllByEndDateTimeNullAndCategory(Category category);
+    List<Activity> findAllByStartDateTimeAfter(ZonedDateTime after);
+    List<Activity> findAllByStartDateTimeAfterAndStartDateTimeBefore(ZonedDateTime after, ZonedDateTime before);
+    List<Activity> findAllByStartDateTimeBefore(ZonedDateTime before);
+    List<Activity> findAllByCategoryAndStartDateTimeAfter(Category category, ZonedDateTime after);
+    List<Activity> findAllByCategoryAndStartDateTimeAfterAndStartDateTimeBefore(Category category, ZonedDateTime
+            after, ZonedDateTime before);
+    List<Activity> findAllByCategoryAndStartDateTimeBefore(Category category, ZonedDateTime before);
 }
