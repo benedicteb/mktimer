445d1a047e69ac4ecb641134abafc1fe832b0208
Benedicte Emilie Brækken <b@brkn.io>
2018-07-02 14:37:15 +0200

    Add tag_release to travis

---
+++ .scripts/tag_release.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  mvn_version=$(mvn -q \
+      -Dexec.executable="echo" \
+      -Dexec.args='${project.version}' \
+      --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+  tag="travistest-v$mvn_version"
+
+  git tag -a $tag -m "Released version $version"
+  git push origin $tag
+fi
