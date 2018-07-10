1c564f36d127d956ec37c0de1a050d6dde10e787
Benedicte Emilie Brækken <b@brkn.io>
2018-07-10 15:12:55 +0200

    Add LiveReload

--- mktimer-frontend/webpack.config.js
+++ mktimer-frontend/webpack.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const SriPlugin = require("webpack-subresource-integrity");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
 
 const env = process.env.NODE_ENV;
+
+let extraHtml = "";
+
+if (env === "development") {
+  extraHtml =
+    extraHtml + '<script src="http://localhost:35729/livereload.js"></script>';
+}
 
 module.exports = {
   mode: env || "development",
@@ -27,11 +35,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       hash: true,
       filename: "index.html",
-      template: "./src/index.html"
+      template: "./src/index.html",
+      extraHtml: extraHtml
     }),
     new SriPlugin({
       hashFuncNames: ["sha256", "sha384", "sha512"],
       enabled: env === "production"
-    })
+    }),
+    new LiveReloadPlugin()
   ]
 };
